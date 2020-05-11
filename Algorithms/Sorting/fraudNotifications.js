965ad99abc6d2c90b440c64f189ae4c4d7933c3e
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-05-11 11:54:32 +0800

    Initiate second attempt

--- Algorithms/Sorting/fraudNotifications.js
+++ Algorithms/Sorting/fraudNotifications.js
@@ -1,57 +1,49 @@
-/*----MY SOLUTION----*/
+/*
+ * Technical coaching assignment for Sorting
+ * https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem
+ */
+
+/*----MY SOLUTION ATTEMPT 2----*/
 function activityNotifications(expenditure, d) {
-    // Create trailing transaction array
-    const trailingArr = []
-    for (let i = 0; i < expenditure.length - d; i++) {
-        let subArr = getTrailingDays(expenditure, i, d);
-        console.log(`subArr ${typeof subArr}`)
-        trailingArr.push(subArr);
-    }
-    console.log(`trailingArr ${trailingArr}`)
+	 
+}
+
+
+/*----MY SOLUTION ATTEMPT 1----*/
+function activityNotifications(expenditure, d) {
 	// Track fraud alerts
 	let fraudAlerts = 0;
 	// Iterate through transaction data
 	for (let i = 0; i < expenditure.length; i++) {
-        // If i is greater than number of trailing days
-        console.log(`trailingArr[i]: ${trailingArr[i]}`);
-		// if (i >= d) {
-		// 	console.log(`I spent $${expenditure[i]} today`);
-		// 	// Slice trailing days from transaction data
-		// 	//let trailingDays = getTrailingDays(expenditure, i, d);
-		// 	// Identify or calculate the median expenditure
-		// 	let medianAmt = getMedian(trailingArr[i]);
-		// 	console.log(`alert threshold ${medianAmt * 2}`);
-		// 	// Compare current expenditure with 2 x median expenditure
-		// 	if (expenditure[i] >= medianAmt * 2) {
-		// 		fraudAlerts++;
-		// 		console.log(
-		// 			`FRAUD ALERT: Spent $${expenditure[i]} >= ${medianAmt * 2}`
-		// 		);
-		// 	}
-		// }
+		// If i is greater than or equal to number of trailing days
+		if (i >= d) {
+			console.log(`I spent $${expenditure[i]} today`);
+			// Slice trailing days from transaction data
+			let trailingDays = getTrailingDays(expenditure, i, d);
+			// Identify or calculate the median expenditure
+			let medianAmt = getMedian(trailingDays);
+			console.log(`alert threshold ${medianAmt * 2}`);
+			// Compare current expenditure with 2 x median expenditure
+			if (expenditure[i] >= medianAmt * 2) {
+				fraudAlerts++;
+				console.log(
+				    `FRAUD ALERT: Spent $${expenditure[i]} >= ${medianAmt * 2}`
+				);
+			}
+		}
 	}
 
 	return fraudAlerts;
 }
 
-// function getTrailingDays(arr, endIndex, trailingDays) {
-// 	const startIndex = endIndex - trailingDays;
-// 	const trailingTransactions = arr
-// 		.slice(startIndex, endIndex)
-// 		.sort((a, b) => a - b);
-// 	console.log(`trailing transactions ${trailingTransactions}`);
-// 	return trailingTransactions;
-// }
-
-function getTrailingDays(arr, startIndex, trailingDays) {
-	const endIndex = startIndex + trailingDays + 1;
+function getTrailingDays(arr, endIndex, trailingDays) {
+	const startIndex = endIndex - trailingDays;
 	const trailingTransactions = arr
 		.slice(startIndex, endIndex)
-		// .sort((a, b) => a - b);
-	console.log(`trailing transactions ${typeof trailingTransactions}`);
+		.sort((a, b) => a - b);
+	console.log(`trailing transactions ${trailingTransactions}`);
 	return trailingTransactions;
 }
-
 
 function getMedian(arr) {
 	let midpoint = Math.floor(arr.length / 2);
